0d71a9660456092bba474e40f4d46f11ba8e40c4
Shamshad Alam <shamshad.npti@gmail.com>
2017-09-14 20:11:01 +0530

    ADD: circleci support

--- init.sh
+++ init.sh
@@ -5,5 +5,7 @@ export SPARK_HOME=/app/spark-2.0.2
 export PATH=$PATH:$KAFKA_HOME/bin:$SPARK_HOME/bin
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 
+echo "$KAFKA_HOME/bin/zookeeper-server-start.sh"
 bash $KAFKA_HOME/bin/zookeeper-server-start.sh -daemon $KAFKA_HOME/config/zookeeper.properties
+sleep 5
 bash $KAFKA_HOME/bin/kafka-server-start.sh -daemon $KAFKA_HOME/config/server.properties
